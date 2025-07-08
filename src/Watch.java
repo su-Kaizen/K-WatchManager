a0c44ffd56011113a4b553d786f05c7160499bd5
su-kaizen <badboynodo313@gmail.com>
2025-07-08 17:22:44 +0200

    feature(add-watch-methods): added toString, some setters, constructors and other attributes

--- src/Watch.java
+++ src/Watch.java
@@ -3,5 +3,34 @@ public class Watch {
     private String brand;
     private String model;
     private String theoreticAccuracy;
+    private String type;
+    public Watch(String b, String m, String t, String c, String ty){
+        brand = b.toUpperCase();
+        model = m;
+        theoreticAccuracy = t;
+        caliber = c;
+        type = ty.toUpperCase();
+    }
+
+    public Watch(String b, String m, String t, String ty){
+        brand = b.toUpperCase();
+        model = m;
+        theoreticAccuracy = t;
+        caliber = "Not specified";
+        type = ty.toUpperCase();
+    }
+
+    public void setBrand(String b){
+        brand = b.toUpperCase();
+    }
+
+    public void setModel(String m){
+        model = m.toUpperCase();
+    }
+
+    @Override
+    public String toString(){
+        return brand+" "+model+" | "+caliber+" | "+theoreticAccuracy;
+    }
 
 }
