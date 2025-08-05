e7c14c2daf150dd604ca0ff4f5b0da565c7cc90c
su-kaizen <badboynodo313@gmail.com>
2025-08-05 12:17:00 +0300

    Fixed everything.

--- src/Manager.java
+++ src/Manager.java
@@ -16,26 +16,20 @@ public class Manager {
 
         decimalFormat = new DecimalFormat("#.##");
         int status = loadWatches();
+        loadColors();
         if(status == -1){
             watches = new ArrayList<>();
 
-        }
-        else if(status == -2){
-            colors = "";
         }
     }
 
     // loads the list of watches and returns -1 if it fails.
     public int loadWatches(){
-        int result = -1;
         try(ObjectInputStream o = new ObjectInputStream(new FileInputStream("watches.bin"))){
             watches = (ArrayList<Watch>) o.readObject();
-            result --;
-            colors = (String) o.readObject();
-            configureColors(colors);
         }
         catch(ClassNotFoundException | IOException ex){
-            return result;
+            return -1;
         }
         return 0;
     }
@@ -44,7 +38,24 @@ public class Manager {
     public void saveWatches(){
         try(ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream("watches.bin"))){
             o.writeObject(watches);
-            o.writeObject(colors);
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void saveColors(){
+        try(BufferedWriter b = new BufferedWriter(new FileWriter("colors.txt"))){
+            b.write(colors);
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void loadColors(){
+        try(BufferedReader b = new BufferedReader(new FileReader("colors.txt"))){
+            Visual.updateColors(b.readLine());
         }
         catch(IOException ex){
             ex.printStackTrace();
@@ -290,28 +301,15 @@ public class Manager {
         System.out.println("Select the color1 and color2 for the program:\n" +
                 Visual.RED+"RED "+Visual.GREEN+"GREEN"+Visual.YELLOW+" YELLOW"+Visual.BLUE+" BLUE"+Visual.CYAN+" CYAN"+Visual.PURPLE+" PURPLE"+Visual.END);
         String choice = getInput(false).toUpperCase();
-        int status = configureColors(choice);
+        int status = Visual.updateColors(choice);
 
         if(status == -1){
             Visual.error();
         }
-    }
-
-    public int configureColors(String colors){
-        String c[] = colors.split("-");
-        if(c.length == 2){
-            int cont = 0;
-            while(cont < 2){
-                switch(c[cont]){
-                    case "RED","CYAN","YELLOW","BLUE","GREEN","PURPLE" ->{Visual.color1 = cont == 0 ? c[cont] : Visual.color1; Visual.color2 = cont == 1 ? c[cont] : Visual.color2;}
-                    default -> {return -1;}
-                }
-                cont ++;
-            }
-            saveWatches();
-            return 0;
+        else{
+            this.colors = choice;
+            saveColors();
+            Visual.success("Successfully changed the colors!");
         }
-
-        return -1;
     }
 }
--- src/Visual.java
+++ src/Visual.java
@@ -13,74 +13,115 @@ public class Visual {
     public static final String CYAN = "\033[0;36m";
     public static final String WHITE = "\033[0;37m";
     public static final String END = "\033[0m";
-    public static final String PIPE = color2+" | "+END;
-    public static final String AT = color2+"@"+color1;
+    public static String PIPE = color2 + " | " + END;
+    public static String AT = color2 + "@" + color1;
 
-    public static void showTitle(){
-        System.out.println(color1+"/////////////////// K-WatchManager v3.0 /////////////////// || github.com/su-Kaizen\n"+END);
+    public static void showTitle() {
+        System.out.println(color1 + "/////////////////// K-WatchManager v3.0 /////////////////// || github.com/su-Kaizen\n" + END);
     }
 
-    public static void showMain(){
-        System.out.println(color1+
-                "[1] Add watch\n" +
+    public static void showMain() {
+        System.out.println(color1 + "[1] Add watch\n" +
                 "[2] Check watch accuracy\n" +
                 "[3] Adjust watch\n" +
-                "[4] Show watch logs\n"+
+                "[4] Show watch logs\n" +
                 "[5] Modify watch data\n" +
                 "[6] Delete watch\n" +
-                "[7] Change program colors" +
+                "[7] Change program colors\n" +
                 "[E] Exit\n" +
-                "Concatenate the option and the ID with and '-', for example to adjust watch 4 type: '3-4'."+END);
+                "Concatenate the option and the ID with and '-', for example to adjust watch 4 type: '3-4'." + END);
     }
 
-    public static void showAddWatch(){
-        System.out.println(color1+"Add the a watch with the following format:\n" +
-                "Brand"+AT+"Model"+AT+"MovementType"+AT+"Caliber"+AT+"TheoreticAccuracy"+AT+"MoreInformation\n" +
-                "If you can not specify one of the fields, just put a '*' instead."+END);
+    public static void showAddWatch() {
+        System.out.println(color1 + "Add the a watch with the following format:\n" + "Brand" + AT + "Model" + AT + "MovementType" + AT + "Caliber" + AT + "TheoreticAccuracy" + AT + "MoreInformation\n" + "If you can not specify one of the fields, just put a '*' instead." + END);
     }
 
-    public static void ask4Time(String now){
-        System.out.println(color1+"What time ("+color2+"hh:mm:ss"+color1+") is it in your watch at: "+color2+now+color1+" ?\nYou can check the exact time at "+color2+"https://time.is"+END);
+    public static void ask4Time(String now) {
+        System.out.println(color1 + "What time (" + color2 + "hh:mm:ss" + color1 + ") is it in your watch at: " + color2 + now + color1 + " ?\nYou can check the exact time at " + color2 + "https://time.is" + END);
     }
 
-    public static void line(){
-        System.out.println(color2+"+-------------------------------------------------------------------------------------------------------------------+"+END);
+    public static void line() {
+        System.out.println(color2 + "+-------------------------------------------------------------------------------------------------------------------+" + END);
     }
 
-    public static void header(){
-        System.out.println(color2+"|  ID  |  BRAND  |  MODEL  |  MOVEMENT  |  CALIBER  |  THEORETIC ACCURACY  |  LAST ADJUSTED  |  MORE INFO"+END);
+    public static void header() {
+        System.out.println(color2 + "|  ID  |  BRAND  |  MODEL  |  MOVEMENT  |  CALIBER  |  THEORETIC ACCURACY  |  LAST ADJUSTED  |  MORE INFO" + END);
     }
 
-    public static void shortHeader(){
-        System.out.println(color2+"BRAND | MODEL | MOVEMENT | CALIBER | THEORETIC ACCURACY | MORE INFO"+END);
+    public static void shortHeader() {
+        System.out.println(color2 + "BRAND | MODEL | MOVEMENT | CALIBER | THEORETIC ACCURACY | MORE INFO" + END);
     }
 
 
-    public static void error(){
-        System.out.println(Visual.RED+"Please type in the correct format..."+Visual.END);
+    public static void error() {
+        System.out.println(Visual.RED + "Please type in the correct format..." + Visual.END);
     }
 
     // Custom error message
-    public static void error(String message){
-        System.out.println(Visual.RED+message+Visual.END);
+    public static void error(String message) {
+        System.out.println(Visual.RED + message + Visual.END);
     }
 
     // Clears the terminal
-    public static void clear(){
+    public static void clear() {
         try {
-            if(System.getProperty("os.name").contains("Windows")){
+            if (System.getProperty("os.name").contains("Windows")) {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            }
-            else{
+            } else {
                 new ProcessBuilder("clear").start();
             }
-        }
-        catch (IOException | InterruptedException ex) {
+        } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
         }
     }
 
-    public static void success(String message){
-        System.out.println(Visual.GREEN+message+Visual.END);
+    public static void success(String message) {
+        System.out.println(Visual.GREEN + message + Visual.END);
+    }
+
+    public static int updateColors(String colors) {
+        String c[] = colors.split("-");
+        if (c.length == 2) {
+            int cont = 0;
+            while (cont < 2) {
+                switch (c[cont]) {
+                    case "RED" -> {
+                        Visual.color1 = cont == 0 ? Visual.RED : Visual.color1;
+                        Visual.color2 = cont == 1 ? Visual.RED : Visual.color2;
+                    }
+                    case "CYAN" -> {
+                        Visual.color1 = cont == 0 ? Visual.CYAN : Visual.color1;
+                        Visual.color2 = cont == 1 ? Visual.CYAN : Visual.color2;
+                    }
+                    case "YELLOW" -> {
+                        Visual.color1 = cont == 0 ? Visual.YELLOW : Visual.color1;
+                        Visual.color2 = cont == 1 ? Visual.YELLOW : Visual.color2;
+                    }
+                    case "BLUE" -> {
+                        Visual.color1 = cont == 0 ? Visual.BLUE : Visual.color1;
+                        Visual.color2 = cont == 1 ? Visual.BLUE : Visual.color2;
+                    }
+                    case "GREEN" -> {
+                        Visual.color1 = cont == 0 ? Visual.GREEN : Visual.color1;
+                        Visual.color2 = cont == 1 ? Visual.GREEN : Visual.color2;
+                    }
+                    case "PURPLE" -> {
+                        Visual.color1 = cont == 0 ? Visual.PURPLE : Visual.color1;
+                        Visual.color2 = cont == 1 ? Visual.PURPLE : Visual.color2;
+                    }
+                    default -> {
+                        return -1;
+                    }
+                }
+                cont++;
+            }
+            // Updating the color icons
+            AT =color2+"@"+color1;
+            PIPE =color2+" | "+END;
+            return 0;
+        }
+
+        return -1;
     }
 }
+
